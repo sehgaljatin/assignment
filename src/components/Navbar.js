32e16cdb777657f7e03a25e88833d4155f22f1a3
Jatin <sehgaljatin16@gmail.com>
2021-09-06 00:15:23 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,28 +8,28 @@ const Navbar = (props) => {
     <>
      <nav className="navbar navbar-expand-lg navbar-light bg-white">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#"><img src="./images/Myntra-logo.png" style={{width: "60px"}} alt=""/></a>
+            <a className="navbar-brand" href="javascript:void(0);"><img src="./images/Myntra-logo.png" style={{width: "60px"}} alt=""/></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                data-target="javascript:void(0);navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">MEN</a>
+                        <a className="nav-link active" aria-current="page" href="javascript:void(0);">MEN</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">WOMEN</a>
+                        <a className="nav-link active" aria-current="page" href="javascript:void(0);">WOMEN</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">WATCHES</a>
+                        <a className="nav-link active" aria-current="page" href="javascript:void(0);">WATCHES</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">CLOTHING</a>
+                        <a className="nav-link active" aria-current="page" href="javascript:void(0);">CLOTHING</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">FOOTWEAR</a>
+                        <a className="nav-link active" aria-current="page" href="javascript:void(0);">FOOTWEAR</a>
                     </li>
                 </ul>
                 <div className="d-flex">
