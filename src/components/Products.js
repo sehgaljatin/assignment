76b8477e71dc158f8462496616e6bcd49799fa19
Jatin <sehgaljatin16@gmail.com>
2021-09-06 11:20:32 +0530

    updated

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,29 +4,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Products = (props) => {
   return (
     <>
-    <div className="cards">
-    <div className="card" style={{width: "235px"}}>
-        <img
-          src={props.img}
-          className="card-img-top"
-          alt="mypic"/>
-        <div className="card-body">
-          <h6 className="card-title">
-            {props.product}
-          </h6>
-          <div className="card-text"><b>{props.brand}</b>
-            <div className="size">{props.sizes}</div>
-            <span className="price">Rs.{props.price}</span>
-            <span className="mrp">Rs.{props.mrp}</span>
-            <span className="discount">{props.dis}</span>
+      <div className="cards">
+        <div className="card" style={{ width: "235px" }}>
+          <img src={props.img} className="card-img-top" alt="mypic" />
+          <div className="card-body">
+            <h6 className="card-title">{props.product}</h6>
+            <div className="card-text">
+              <b>{props.brand}</b>
+              <div className="size">{props.sizes}</div>
+              <span className="price">Rs.{props.price}</span>
+              <span className="mrp">Rs.{props.mrp}</span>
+              <span className="discount">{props.dis}</span>
+            </div>
+            <button className="btn btn-primary">Add to Cart</button>
           </div>
-          <a href="#" className="btn btn-primary">
-            Add to Cart
-          </a>
         </div>
       </div>
-    </div>
-
     </>
   );
 };
